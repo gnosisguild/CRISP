34968b0a07ab427136e1946cd9f800f993bdb179
nginnever <ginneversource@gmail.com>
2024-02-26 18:21:54 +0000

    chacha seed

--- src/bin/generate_validator_keyshare.rs
+++ src/bin/generate_validator_keyshare.rs
@@ -7,7 +7,8 @@ use fhe::{
     mbfv::{AggregateIter, CommonRandomPoly, DecryptionShare, PublicKeyShare},
 };
 use fhe_traits::{FheDecoder, FheEncoder, FheEncrypter, Serialize};
-use rand::{distributions::Uniform, prelude::Distribution, rngs::OsRng, thread_rng};
+use rand::{distributions::Uniform, prelude::Distribution, rngs::OsRng, thread_rng, SeedableRng};
+use rand_chacha::ChaCha8Rng;
 use util::timeit::{timeit, timeit_n};
 //use serde::{Deserialize, Serialize};
 
@@ -21,6 +22,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let plaintext_modulus: u64 = 4096;
     let moduli = vec![0xffffee001, 0xffffc4001, 0x1ffffe0001];
 
+    let mut seed = <ChaCha8Rng as SeedableRng>::Seed::default();
+
     // Let's generate the BFV parameters structure.
     let params = timeit!(
         "Parameters generation",
@@ -33,7 +36,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let crp = CommonRandomPoly::new(&params, &mut thread_rng())?;
     let mut rng = thread_rng();
     println!("{:?}", rng);
-    let test = crp.to_bytes();
+    //let test = crp.to_bytes();
 
     // Party setup: each party generates a secret key and shares of a collective
     // public key.
