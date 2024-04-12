51def7d6d72ab2807bcffb11b23633df0cca41ce
Colin Spence <udappia@gmail.com>
2024-04-12 08:57:38 -0600

    updates

--- packages/client/src/pages/Landing/components/Hero.tsx
+++ packages/client/src/pages/Landing/components/Hero.tsx
@@ -17,7 +17,7 @@ const HeroSection: React.FC = () => {
       <div className='absolute bottom-px right-0 w-[50vh] space-y-2'>
         <CircularTiles count={2} />
       </div>
-      <div className='mx-auto w-full max-w-screen-md space-y-12'>
+      <div className='relative mx-auto w-full max-w-screen-md space-y-12'>
         <div className='space-y-4'>
           <h3 className='text-3xl font-normal leading-none text-zinc-400'>Introducing</h3>
           <img src={Logo} alt='CRISP Logo' className='h-20' />
@@ -52,7 +52,7 @@ const HeroSection: React.FC = () => {
           </button>
           <a
             href='/about'
-            className='flex cursor-pointer items-center space-x-2 text-green-dark-900 duration-300 ease-in-out hover:opacity-70'
+            className='inline-flex cursor-pointer items-center space-x-2 text-green-dark-900 duration-300 ease-in-out hover:opacity-70'
           >
             <ArrowSquareOut size={20} weight='bold' />
             <div>Learn more.</div>
