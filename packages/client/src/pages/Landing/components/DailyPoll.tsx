564bd5cc7ebf75c8f2c038110a6196c4eec16d31
Colin Spence <udappia@gmail.com>
2024-04-12 09:23:03 -0600

    Adds live tag to daily poll section

--- packages/client/src/pages/Landing/components/DailyPoll.tsx
+++ packages/client/src/pages/Landing/components/DailyPoll.tsx
@@ -30,12 +30,21 @@ const DailyPollSection: React.FC<DailyPollSectionProps> = ({ isScreen, onVoted }
   return (
     <>
       <div
-        className={`flex w-screen flex-col items-center justify-center px-6 ${!isScreen ? 'h-screen  border-y-2 border-twilight-blue-200' : 'h-screen-minus-header-footer'} `}
+        className={`flex w-screen flex-col items-center justify-center px-6 ${!isScreen ? 'h-screen' : 'h-screen-minus-header-footer'} `}
       >
         <div className='mx-auto flex w-full max-w-screen-md flex-col items-center justify-center space-y-12'>
           <div className='space-y-2'>
             <p className='text-center text-sm font-extrabold uppercase text-zinc-900'>Daily Poll</p>
-            <h3 className='font-bold text-twilight-blue-900'>Choose your favorite</h3>
+            <h3 className='font-bold leading-none text-twilight-blue-900'>Choose your favorite</h3>
+          </div>
+          <div className='flex items-center justify-center space-x-2'>
+            <div className='flex items-center space-x-2 rounded-lg border-2 border-green-dark-800 bg-green-light px-2 py-1 text-center font-bold uppercase leading-none text-white-900'>
+              <div className='h-1.5 w-1.5 animate-pulse rounded-full bg-white-900'></div>
+              <div>Live</div>
+            </div>
+            <div className='rounded-lg border-2 border-twilight-blue-200 bg-white-900 px-2 py-1.5 text-center font-bold uppercase leading-none text-zinc-500'>
+              23 votes
+            </div>
           </div>
           <div className='grid w-full grid-cols-2 gap-4 md:gap-8'>
             {pollOptions.map((poll) => (
