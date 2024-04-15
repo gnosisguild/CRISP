5943132855fac7d2c5958440907b97498f12c676
nginnever <ginneversource@gmail.com>
2024-04-15 17:56:03 +0000

    remove imports

--- packages/rust/src/bin/web_test.rs
+++ packages/rust/src/bin/web_test.rs
@@ -1,41 +1,19 @@
 mod util;
 
-use dialoguer::{theme::ColorfulTheme, Input, FuzzySelect};
 use wasm_bindgen::prelude::*;
 
 use std::{thread, time, env, sync::Arc};
 use serde::Deserialize;
-use std::fs::File;
-use std::io::Read;
 
 use fhe::{
     bfv::{BfvParametersBuilder, Ciphertext, Encoding, Plaintext, PublicKey, SecretKey},
     mbfv::{AggregateIter, CommonRandomPoly, DecryptionShare, PublicKeyShare},
 };
 use fhe_traits::{FheDecoder, FheEncoder, FheEncrypter, Serialize, DeserializeParametrized};
-//use fhe_math::rq::{Poly};
 use rand::{distributions::Uniform, prelude::Distribution, rngs::OsRng, thread_rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
 use util::timeit::{timeit, timeit_n};
-
-use http_body_util::Empty;
-use hyper::Request;
-//use hyper::body::Bytes;
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpStream;
-use http_body_util::BodyExt;
-use tokio::io::{AsyncWriteExt as _, self};
 use rustc_serialize::json;
-
-use ethers::{
-    prelude::{abigen, Abigen},
-    providers::{Http, Provider},
-    middleware::SignerMiddleware,
-    signers::{LocalWallet, Signer, Wallet},
-    types::{Address, U256, Bytes},
-    core::k256,
-    utils,
-};
 
 #[wasm_bindgen]
 pub struct Encrypt {
@@ -75,7 +53,6 @@ impl Encrypt {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     Ok(())
 }
