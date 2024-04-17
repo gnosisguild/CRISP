3586e9a50ab12aad6b1195d3becdad2e74a88468
juliopavila <javila1997@gmail.com>
2024-04-17 18:18:58 -0300

    feat: add hash router

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './globals.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { VoteManagementProvider } from '@/context/voteManagement/index.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <VoteManagementProvider>
         <App />
       </VoteManagementProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
