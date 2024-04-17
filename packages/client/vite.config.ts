1c53e947f2bb8f40316a20488e35a38644b32391
juliopavila <javila1997@gmail.com>
2024-04-17 17:45:50 -0300

    chore: change vite config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -9,7 +9,7 @@ import path from 'path'
 const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 export default defineConfig({
-  base: development ? '' : '/CRISP',
+  base: development ? '' : '/CRISP/',
   define: {
     // here is the main update
     global: 'globalThis',
