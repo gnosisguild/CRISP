c448b675a46121b84f0dff7b84ce5e65a23aae1a
juliopavila <javila1997@gmail.com>
2024-04-18 16:02:23 -0300

    feat: create toast alert component

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -25,9 +25,10 @@ const config = {
       boxShadow: {
         button: '0 2px 0 0 #5F9715, 0 8px 16px rgba(0,0,0,0.1)',
         'button-outlined': '0 2px 0 0 #A6E05A, 0 8px 16px rgba(0,0,0,0.1)',
+        danger: '0 2px 0 0 #EF4444, 0 8px 16px rgba(0,0,0,0.1)',
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'),]
+  plugins: [require('@tailwindcss/typography')],
 }
 export default config
