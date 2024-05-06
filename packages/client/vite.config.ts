c7166ac05543a57ae67b84790035e31ed3c40b9b
juliopavila <javila1997@gmail.com>
2024-05-06 17:23:20 -0300

    chore: remove /CRISP prefix

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -9,7 +9,7 @@ import path from 'path'
 // const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 export default defineConfig({
-  base: '/CRISP/',
+  base: '/',
   define: {
     // here is the main update
     global: 'globalThis',
