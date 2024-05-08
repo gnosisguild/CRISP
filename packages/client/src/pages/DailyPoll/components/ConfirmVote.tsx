3fc28d47c319ce249283bd6a3e9df15ccd153b6d
Auryn Macmillan <hi@auryn.xyz>
2024-05-08 03:47:24 +0000

    copy edits on confirmation page

--- packages/client/src/pages/DailyPoll/components/ConfirmVote.tsx
+++ packages/client/src/pages/DailyPoll/components/ConfirmVote.tsx
@@ -22,11 +22,9 @@ const ConfirmVote: React.FC<ConfirmVoteProps> = ({ endTime }) => {
           <p className='text-base font-extrabold uppercase text-slate-600/50'>WHAT JUST HAPPENED?</p>
           <div className='space-y-2'>
             <p className='text-xl leading-8 text-slate-600'>
-              After casting your vote, CRISP securely processed your selection using a blend of Fully Homomorphic Encryption (FHE),
-              threshold cryptography, and zero-knowledge proofs (ZKPs), without revealing your identity or choice. Your vote was encrypted
-              and anonymously aggregated with others, ensuring the integrity of the voting process while strictly maintaining
-              confidentiality. The protocol's advanced cryptographic techniques guarantee that your vote contributes to the final outcome
-              without any risk of privacy breaches or undue influence.
+              Your vote was encrypted and posted onchain by a relayer. When the poll is over, the results will be tallied using
+              Fully Homomorphic Encryption (FHE) and the results decrypted using threshold cryptography, without revealing your
+              identity or choice.
             </p>
           </div>
         </div>
