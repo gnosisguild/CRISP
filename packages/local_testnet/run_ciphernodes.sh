1aa2d92afb8195ddd9d94470b20daa0f76961ed7
Hamza Khalid <hamzakhalid829@gmail.com>
2024-10-03 19:39:08 +0500

    Update Scripts

--- packages/local_testnet/run_ciphernodes.sh
+++ packages/local_testnet/run_ciphernodes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Set common environment variables
-export RPC_URL="ws://localhost:8545"
-export ENCLAVE_CONTRACT="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-export REGISTRY_CONTRACT="0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+RPC_URL="ws://localhost:8545"
+ENCLAVE_CONTRACT="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+REGISTRY_CONTRACT="0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
 
 # Function to run ciphernode
 run_ciphernode() {
